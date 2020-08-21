aa79974d8931192418e42d6372a6cd6261269aa0
prk <prk@bn71115.local>
2020-08-21 10:56:15 +0700

    Initial Commit

---
+++ MyNoveliSK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNoveliSK
+//
+//  Created by prk on 21/08/20.
+//  Copyright © 2020 prk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
